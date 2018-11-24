1a52d310a9126aad7de76b6ad03d068940d0bf44
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-23 21:35:29 -0800

    Convert to MVC design pattern

---
+++ ToDoApp/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoApp
+//
+//  Created by Dontrell Washington on 11/23/18.
+//  Copyright © 2018 Dontrell Washington. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
