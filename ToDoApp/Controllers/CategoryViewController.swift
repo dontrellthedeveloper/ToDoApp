e12c813abcc2dc825af49e66db84556a9a905e01
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-25 19:12:05 -0800

    Added Swipe Delete capability

--- ToDoApp/Controllers/CategoryViewController.swift
+++ ToDoApp/Controllers/CategoryViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
@@ -20,7 +20,6 @@ class CategoryViewController: UITableViewController {
         
          loadCategories()
 
-
     }
 
     //MARK: - TableView Datasource Methods
@@ -30,11 +29,16 @@ class CategoryViewController: UITableViewController {
         return categories?.count ?? 1
     }
     
+    
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+
         
         return cell
         
@@ -80,6 +84,20 @@ class CategoryViewController: UITableViewController {
         
     }
     
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
     
     //MARK: - Add New Categories
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -110,9 +128,7 @@ class CategoryViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
         
     }
-    
 
-    
-    
-    
 }
+
+
--- ToDoApp/Controllers/ToDoListViewController.swift
+++ ToDoApp/Controllers/ToDoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: SwipeTableViewController {
 
     var todoItems: Results<Item>?
     let realm = try! Realm()
@@ -35,10 +35,7 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -47,7 +44,6 @@ class ToDoListViewController: UITableViewController {
         } else {
             cell.textLabel?.text = "No Items Added"
         }
-        
         
         return cell
     }
@@ -121,6 +117,18 @@ class ToDoListViewController: UITableViewController {
 
             tableView.reloadData()
         }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row] {
+            do {
+            try realm.write {
+                realm.delete(item)
+            }
+            }catch {
+                print("Error deleting Item, \(error)")
+            }
+        }
+    }
     
 
     
